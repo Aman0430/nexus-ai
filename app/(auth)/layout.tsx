127702cee79184dbf14569f400b13460f89562d0
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2024-07-05 21:46:06 +0530

    initial commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center h-full bg-primary">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const DashboardPage = (props: Props) => {
+  return (
+    <div>
+      <p>DashboardPage(protected)</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
